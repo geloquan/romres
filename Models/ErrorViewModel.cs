d31d93497fe7832f51c68abe779cf392cdbde00d
IKHUpy <72260774+IKHUpy@users.noreply.github.com>
2024-04-16 18:30:53 +0800

    fixed classes

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -7,74 +7,39 @@ namespace WebApplication2.Models {
     }
     
     public class Host {
-        public int HostId { get; set; }
+        public int Id { get; set; }
         public string HostName { get; set; }
-    }
 
-    public class Scope
-    {
-        public int ScopeId { get; set; }
-        public int HostId { get; set; }
-        public string ScopeName { get; set; }
-
-        // Navigation property
-        public Host Host { get; set; }
-    }
-
-    public class Level
-    {
-        public int LevelId { get; set; }
-        public int ScopeId { get; set; }
-        public string LevelName { get; set; }
-
-        // Navigation property
         public Scope Scope { get; set; }
     }
 
-    public class IndexLevel
-    {
-        public int IndexLevelId { get; set; }
-        public int LevelId { get; set; }
-        public string IndexLevelName { get; set; }
+    public class Scope {
+        public int Id { get; set; }
 
-        // Navigation property
-        public Level Level { get; set; }
+        public List<Level> Levels { get; set; }
     }
 
-    public class Reservee
-    {
-        public int ReserveeId { get; set; }
-        public string ReserveeName { get; set; }
+    public class Level {
+        public int Id { get; set; }
+        public int IndexLevel { get; set; }
+        public int Reserver { get; set; }
+
+        public List<Slot> Slots { get; set; }
     }
 
-    public class Slot
-    {
-        public int SlotId { get; set; }
-        public int IndexLevelId { get; set; }
-        public DateTime SlotDateTime { get; set; }
-
-        // Navigation property
-        public IndexLevel IndexLevel { get; set; }
+    public class Slot {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public List<Edge> Edges { get; set; }
+        public string Reserver { get; set; }
+        public List<DateTime> DateTimes { get; set; }
     }
-
-    public class Edge
-    {
-        public int EdgeId { get; set; }
-        public int SlotId { get; set; }
-        public int EdgeX { get; set; }
-        public int EdgeY { get; set; }
-
-        // Navigation property
-        public Slot Slot { get; set; }
+    public class Edge {
+        public int X { get; set; }
+        public int Y { get; set; }
     }
-
-    public class SlotReservee
-    {
-        public int SlotId { get; set; }
-        public int ReserveeId { get; set; }
-
-        // Navigation properties
-        public Slot Slot { get; set; }
-        public Reservee Reservee { get; set; }
+    public class DateTime {
+        public int Date { get; set; }
+        public int Reserver { get; set; }
     }
 }

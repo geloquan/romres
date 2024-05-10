14fc5a2b0dc76f51f9c7f9a28d60837c7bde9562
IKHUpy <72260774+IKHUpy@users.noreply.github.com>
2024-05-10 16:24:33 +0800

    model for editing fields

--- Models/SlotModel.cs
+++ Models/SlotModel.cs
@@ -18,19 +18,21 @@ namespace WebApplication2.Models {
         }
     }
     public class HttpPutSlotEdit {
-        public int? UserId { get; set; }
-        public bool? Reserve { get; set; }
+        public int? SlotId { get; set; }
+        public bool? IsReservable { get; set; }
+        public string? InvitationCode { get; set; }
+        public string? Note { get; set; }
         public bool Process() {
+            Console.Write("HttpPutSlotEdit.Process()");
             string query = @"
                 
             ";
             try {
-                
                 return true;
             } 
             catch (Exception ex)
             {
-                Console.WriteLine($"Error processing HttpPutReserve: {ex.Message}");
+                Console.WriteLine($"Error processing HttpPutSlotEdit: {ex.Message}");
                 return false;  
             }
         }

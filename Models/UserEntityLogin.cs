112aa6420f9adb69e42ec86e56426b71250d4a47
IKHUpy <72260774+IKHUpy@users.noreply.github.com>
2024-05-15 17:14:42 +0800

    triggering the function that will count the number of fave slots to be return of one of the values for a request.

--- Models/UserEntityLogin.cs
+++ Models/UserEntityLogin.cs
@@ -31,25 +31,31 @@ namespace WebApplication2.Models {
         }
         public bool DirectLogin(int UserId) {
             string query = "SELECT id, name, password FROM dbo.[user] WHERE id = @user_id;";
+            GetFavoriteSlotCount(UserId);
+            bool loginSuccess = false;
             try {
                 string ConnectionQuery = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=rom;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                
                 using (SqlConnection conn = new SqlConnection(ConnectionQuery)) {
                     using (SqlCommand command = new SqlCommand(query, conn)) {
-                        command.Parameters.Add("@user_id", System.Data.SqlDbType.Int, 50).Value = UserId;
+                        command.Parameters.Add("@user_id", System.Data.SqlDbType.Int).Value = UserId;
                         conn.Open();
-                        using (SqlDataReader reader = command.ExecuteReader()){
-                            while (reader.Read()) {
+                        using (SqlDataReader reader = command.ExecuteReader()) {
+                            if (reader.Read()) {
                                 this.Id = reader.GetInt32(0);
                                 this.Name = reader.GetString(1);
                                 this.Password = reader.GetString(2);
+                                loginSuccess = true;
                             } 
-                        };
-                    };
-                };
+                            reader.Close();
+                        }
+                    }
+                }
             } 
             catch (Exception e) {
                 Console.WriteLine("UserEntityLogin.DirectLogin() : " + e.Message);
             }
+            return loginSuccess; 
         }
         public void Verify(LoginModel loginModel) {
             string query = "SELECT * FROM dbo.[user] WHERE name = @name AND password = @password;";

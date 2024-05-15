60999e5502984f3a798b107d9a3f28708859a917
IKHUpy <72260774+IKHUpy@users.noreply.github.com>
2024-05-15 16:59:41 +0800

    init and for creating new host with necessary property values

--- Controllers/HostController.cs
+++ Controllers/HostController.cs
@@ -7,12 +7,30 @@ namespace WebApplication2.Controllers {
         public IActionResult Index() {
             return View();
         }
-
         public IActionResult Id(int host_id) {
             return View();
         }
         public IActionResult Slot(int host_id, int slot_id) {
             return View();
+        }
+        [HttpPut("/host/newhost")]
+        public IActionResult NewHost(int userId, [FromBody] HttpPutNewHost newHostRequest) {
+            Console.WriteLine("NewHost()");
+            if (newHostRequest != null) {
+                bool processingResult = newHostRequest.Process();
+                if (processingResult) {
+                    var result = new {
+                        response = "Reservation successful",
+                        new_slot_id = newHostRequest.newSlotId
+                    };
+                    return new JsonResult(result);
+                }
+                else {
+                    return StatusCode(500, "Failed to process new host.");  // Return appropriate error status
+                }
+            } else {
+                return StatusCode(500, "Invalid request data.");
+            }
         }
     }
 }
